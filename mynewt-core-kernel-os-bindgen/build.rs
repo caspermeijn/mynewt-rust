2d055f0cdcc031ebbdfbf92e7aecf1b50d148712
Casper Meijn <casper@meijn.net>
2020-07-24 17:14:52 +0200

    Add mynewt-core-kernel-os for Delay

---
+++ mynewt-core-kernel-os-bindgen/build.rs
@@ -0,0 +1,52 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2020 Casper Meijn <casper@meijn.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+extern crate mynewt_bindgen_helper;
+
+fn main() {
+    let header_files = vec!(
+        "kernel/os/include/os/endian.h",
+        "kernel/os/include/os/mynewt.h",
+        "kernel/os/include/os/os.h",
+        "kernel/os/include/os/os_callout.h",
+        "kernel/os/include/os/os_cfg.h",
+        "kernel/os/include/os/os_cputime.h",
+        "kernel/os/include/os/os_dev.h",
+        "kernel/os/include/os/os_error.h",
+        "kernel/os/include/os/os_eventq.h",
+        "kernel/os/include/os/os_fault.h",
+        "kernel/os/include/os/os_heap.h",
+        "kernel/os/include/os/os_malloc.h",
+        "kernel/os/include/os/os_mbuf.h",
+        "kernel/os/include/os/os_mempool.h",
+        "kernel/os/include/os/os_mutex.h",
+        "kernel/os/include/os/os_sanity.h",
+        "kernel/os/include/os/os_sched.h",
+        "kernel/os/include/os/os_sem.h",
+        "kernel/os/include/os/os_task.h",
+        "kernel/os/include/os/os_time.h",
+        "kernel/os/include/os/os_trace_api.h",
+        "kernel/os/include/os/queue.h",
+        "kernel/os/include/os/util.h",
+    );
+
+    let result = mynewt_bindgen_helper::generate(header_files);
+    if let Err(e) = result {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    };
+}
---
+++ mynewt-core-kernel-os/src/external_hal/blocking.rs
@@ -0,0 +1,34 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright 2020 Casper Meijn <casper@meijn.net>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use embedded_hal::blocking::delay::DelayMs;
+
+impl embedded_hal::blocking::delay::DelayMs<u32> for crate::time::Delay {
+    fn delay_ms(&mut self, ms: u32) {
+        let mut ticks: mynewt_core_kernel_os_bindgen::os_time_t = 0;
+        let result = unsafe { mynewt_core_kernel_os_bindgen::os_time_ms_to_ticks(ms, &mut ticks) };
+        assert!(result == 0);
+        unsafe { mynewt_core_kernel_os_bindgen::os_time_delay(ticks) };
+    }
+}
+
+impl embedded_hal::blocking::delay::DelayUs<u32> for crate::time::Delay {
+    fn delay_us(&mut self, us: u32) {
+        unsafe { mynewt_core_kernel_os_bindgen::os_cputime_delay_usecs(us) };
+    }
+}
+
