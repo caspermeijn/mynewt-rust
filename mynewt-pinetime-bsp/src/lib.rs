d7d66ff764b8368234cf33273f0f8a96b42f3e34
Casper Meijn <casper@meijn.net>
2020-07-31 13:51:26 +0200

    Use obtainable peripherals for BSP

--- mynewt-pinetime-bsp/src/lib.rs
+++ mynewt-pinetime-bsp/src/lib.rs
@@ -24,18 +24,29 @@ use mynewt_core_kernel_os::time::Delay;
 mod binding;
 
 pub struct Bsp {
-    pub backlight_low: OutputPin,
-    pub backlight_medium: OutputPin,
-    pub backlight_high: OutputPin,
-    pub display_chip_select: OutputPin,
-    pub display_data_command: OutputPin,
-    pub display_reset: OutputPin,
-    pub delay: Delay,
-    pub spi: Spi,
+    pub backlight_low: Option<OutputPin>,
+    pub backlight_medium: Option<OutputPin>,
+    pub backlight_high: Option<OutputPin>,
+    pub display_chip_select: Option<OutputPin>,
+    pub display_data_command: Option<OutputPin>,
+    pub display_reset: Option<OutputPin>,
+    pub spi: Option<Spi>,
 }
 
 impl Bsp {
-    pub fn new() -> Bsp {
+    pub const fn new() -> Bsp {
+        Bsp {
+            backlight_low: None,
+            backlight_medium: None,
+            backlight_high: None,
+            display_chip_select: None,
+            display_data_command: None,
+            display_reset: None,
+            spi: None
+        }
+    }
+
+    pub fn init(&mut self) {
         let backlight_low_pin = unsafe{Gpio::new(binding::LCD_BACKLIGHT_LOW_PIN as i32)};
         let backlight_medium_pin = unsafe{Gpio::new(binding::LCD_BACKLIGHT_MED_PIN as i32)};
         let backlight_high_pin = unsafe{Gpio::new(binding::LCD_BACKLIGHT_HIGH_PIN as i32)};
@@ -43,20 +54,17 @@ impl Bsp {
         let display_chip_select_pin = unsafe{Gpio::new(binding::LCD_CHIP_SELECT_PIN as i32)};
         let display_data_command_pin = unsafe{Gpio::new(binding::LCD_WRITE_PIN as i32)};
         let display_reset_pin = unsafe{Gpio::new(binding::LCD_RESET_PIN as i32)};
-        
+
         let mut spi = unsafe {Spi::new(0)};
 
         spi.config(DataMode::Mode3, DataOrder::MsbFirst, WordSize::Size8bit, 8000);
 
-        Bsp {
-            backlight_low: backlight_low_pin.init_as_output().unwrap(),
-            backlight_medium: backlight_medium_pin.init_as_output().unwrap(),
-            backlight_high: backlight_high_pin.init_as_output().unwrap(),
-            display_chip_select: display_chip_select_pin.init_as_output().unwrap(),
-            display_data_command: display_data_command_pin.init_as_output().unwrap(),
-            display_reset: display_reset_pin.init_as_output().unwrap(),
-            delay: Delay{},
-            spi,
-        }
+        self.backlight_low = Some(backlight_low_pin.init_as_output().unwrap());
+        self.backlight_medium = Some(backlight_medium_pin.init_as_output().unwrap());
+        self.backlight_high = Some(backlight_high_pin.init_as_output().unwrap());
+        self.display_chip_select = Some(display_chip_select_pin.init_as_output().unwrap());
+        self.display_data_command = Some(display_data_command_pin.init_as_output().unwrap());
+        self.display_reset = Some(display_reset_pin.init_as_output().unwrap());
+        self.spi = Some(spi);
     }
 }
