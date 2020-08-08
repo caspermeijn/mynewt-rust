74d2699312b3819de843761088cff748bde1e491
Casper Meijn <casper@meijn.net>
2020-08-08 14:14:43 +0200

    pinetime-bsp: Use SpiNode display

--- mynewt-pinetime-bsp/src/lib.rs
+++ mynewt-pinetime-bsp/src/lib.rs
@@ -18,7 +18,8 @@
 #![no_std]
 
 use mynewt_core_hw_hal::gpio::{Gpio, OutputPin};
-use mynewt_core_hw_hal::spi::{DataMode, DataOrder, Spi, WordSize};
+use mynewt_core_hw_hal::spi::{DataMode, DataOrder, WordSize};
+use mynewt_core_hw_bus::spi::SpiNode;
 use mynewt_core_kernel_os::time::Delay;
 
 mod binding;
@@ -27,10 +28,9 @@ pub struct Bsp {
     pub backlight_low: Option<OutputPin>,
     pub backlight_medium: Option<OutputPin>,
     pub backlight_high: Option<OutputPin>,
-    pub display_chip_select: Option<OutputPin>,
     pub display_data_command: Option<OutputPin>,
     pub display_reset: Option<OutputPin>,
-    pub spi: Option<Spi>,
+    pub display_spi: Option<SpiNode>,
 }
 
 impl Bsp {
@@ -39,37 +39,28 @@ impl Bsp {
             backlight_low: None,
             backlight_medium: None,
             backlight_high: None,
-            display_chip_select: None,
             display_data_command: None,
             display_reset: None,
-            spi: None,
+            display_spi: None,
         }
     }
 
-    pub fn init(&mut self) {
+    pub fn init(&'static mut self) {
         let backlight_low_pin = unsafe { Gpio::new(binding::LCD_BACKLIGHT_LOW_PIN as i32) };
         let backlight_medium_pin = unsafe { Gpio::new(binding::LCD_BACKLIGHT_MED_PIN as i32) };
         let backlight_high_pin = unsafe { Gpio::new(binding::LCD_BACKLIGHT_HIGH_PIN as i32) };
 
-        let display_chip_select_pin = unsafe { Gpio::new(binding::LCD_CHIP_SELECT_PIN as i32) };
         let display_data_command_pin = unsafe { Gpio::new(binding::LCD_WRITE_PIN as i32) };
         let display_reset_pin = unsafe { Gpio::new(binding::LCD_RESET_PIN as i32) };
+        let mut display_spi = unsafe { SpiNode::new("spidisplay\0") };
 
-        let mut spi = unsafe { Spi::new(0) };
-
-        spi.config(
-            DataMode::Mode3,
-            DataOrder::MsbFirst,
-            WordSize::Size8bit,
-            8000,
-        );
+        display_spi.open();
 
         self.backlight_low = Some(backlight_low_pin.init_as_output().unwrap());
         self.backlight_medium = Some(backlight_medium_pin.init_as_output().unwrap());
         self.backlight_high = Some(backlight_high_pin.init_as_output().unwrap());
-        self.display_chip_select = Some(display_chip_select_pin.init_as_output().unwrap());
         self.display_data_command = Some(display_data_command_pin.init_as_output().unwrap());
         self.display_reset = Some(display_reset_pin.init_as_output().unwrap());
-        self.spi = Some(spi);
+        self.display_spi = Some(display_spi);
     }
 }
