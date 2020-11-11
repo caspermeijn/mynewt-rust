37da3158b3db622f72ba2ecc35e755138faf78f7
Casper Meijn <casper@meijn.net>
2020-11-11 21:54:47 +0100

    build: Use build profile to choose release or debug build

--- apps/blinky/cargo_build.sh
+++ apps/blinky/cargo_build.sh
@@ -33,5 +33,12 @@ else
   exit 1
 fi
 
-cargo +nightly build --target="${TARGET}" --target-dir="${MYNEWT_PKG_BIN_DIR}"
-cp "${MYNEWT_PKG_BIN_DIR}"/${TARGET}/debug/*.a "${MYNEWT_PKG_BIN_ARCHIVE}"
+BUILD_PROFILE="${MYNEWT_BUILD_PROFILE-default}"
+
+if [ "${BUILD_PROFILE}" = "optimized" ] || [ "${BUILD_PROFILE}" = "speed" ]; then
+  cargo +nightly build --release --target="${TARGET}" --target-dir="${MYNEWT_PKG_BIN_DIR}"
+  cp -p "${MYNEWT_PKG_BIN_DIR}"/${TARGET}/release/*.a "${MYNEWT_PKG_BIN_ARCHIVE}"
+else
+  cargo +nightly build --target="${TARGET}" --target-dir="${MYNEWT_PKG_BIN_DIR}"
+  cp -p "${MYNEWT_PKG_BIN_DIR}"/${TARGET}/debug/*.a "${MYNEWT_PKG_BIN_ARCHIVE}"
+fi
